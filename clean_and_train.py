7f003d7ed82b3cd3b83b371f6d1ef509fe780440
FilipyStudy <fiilipydasilvafurtado@gmail.com>
2024-12-31 18:26:44 -0300

    fix: removed some unecessairy code

--- clean_and_train.py
+++ clean_and_train.py
@@ -2,69 +2,67 @@ import pandas as pd
 import matplotlib as plt
 import sklearn
 import re
-from pathlib import path
+from pathlib import Path
 import random
 import numpy as np
 import nltk
 from nltk.tokenize import word_tokenize
+import gc
 
-#not random stuff
-DB_PATH = path('database')
+#Not random stuff
+DB_PATH = Path('database')
 REGEX_NUMBER = re.compile('[0-3]')
-REGEX_PUNCTUATION = re.compile('[a-zA-Z ]')
+REGEX_PUNCTUATION = re.compile('[A-Za-z ]')
 stopwords = nltk.corpus.stopwords.words('english')
 
-#regex function to get numbers
+#Regex function to get numbers
 def regex_number(x):
     value = REGEX_NUMBER.findall(x)
     return str(value[0])
 
-#regex function to get out the punctuation
+#Regex function to get out the punctuation
 def regex_punctuation(x):
     value = REGEX_PUNCTUATION.findall(x)
     return "".join([i for i in value]).lower()
     
 
-#define the lambda function to remove stop words from the text
+#Define the lambda function to remove stop words from the text
 remove_stopwords = lambda x: [word for word in x if word not in stopwords]
 
-#extract, transform and load the data. aka etl.
+#Extract, Transform and Load the data. Aka ETL.
 try:
     if DB_PATH.exists():
         train_file = open(r'database/train_data.txt', 'r')
-        test_file = open(r'database/test_data.txt', 'r')
 
-        train_dataframe = pd.read_fwf(train_file, sep=',', header=none, encoding='utf-8')
-        test_dataframe = pd.read_fwf(test_file, sep=',', header=none, encoding='utf-8')
-       
-        #close the database to save memory and prevent oom on my os
+        train_dataframe = pd.read_fwf(train_file, sep=',', header=None, encoding='utf-8')
+        
+        #Close the files and clean the memory to prevent OOM
         train_file.close()
-        test_file.close()
-        #remove nan column
+        gc.collect()
+
+        #Remove NaN column
         train_dataframe = train_dataframe.drop(2, axis=1)
-
-#rename the header of the dataframe.
-    headers = ['rating', 'review']
-    train_dataframe.rename({0: headers[0], 1: headers[1]}, axis=1, inplace=true)
-    test_dataframe.rename({0: headers[0], 1: headers[1]}, axis=1, inplace=true)
-
-    #clean the dataframe.
-    train_dataframe['rating'] = train_dataframe['rating'].apply(regex_number)
-    test_dataframe['rating'] = test_dataframe['rating'].apply(regex_number)
-    train_dataframe['review'] = train_dataframe['review'].apply(regex_punctuation)
-    test_dataframe['review'] = test_dataframe['review'].apply(regex_punctuation)
-
-    #tokenize the text
-    train_dataframe['review'] = train_dataframe['review'].apply(word_tokenize)
-    test_dataframe['review'] = test_dataframe['review'].apply(word_tokenize)
     
-    #remove stop words.
+#Rename the header of the DataFrame.
+    headers = ['rating', 'review']
+    train_dataframe.rename({0: headers[0], 1: headers[1]}, axis=1, inplace=True)
+
+    print('Applying func: regex_number and regex_punctuation')
+    #Clean the DataFrame.
+    train_dataframe['rating'] = train_dataframe['rating'].apply(regex_number)
+    train_dataframe['review'] = train_dataframe['review'].apply(regex_punctuation)
+
+
+    print('Applying func: word_tokenize from NLTK')
+    #Tokenize the text
+    train_dataframe['review'] = train_dataframe['review'].apply(word_tokenize)
+    
+    print('Applying func: remove_stopwords')
+    #Remove Stop Words.
     train_dataframe['review'] = train_dataframe['review'].apply(remove_stopwords)
-    test_dataframe['review'] = test_dataframe['review'].apply(remove_stopwords)
 
-    #print the dataframe for debugging.
+    #Print the DataFrame for debugging.
     print(train_dataframe.head())
-    print(test_dataframe.head())
 
-except exception as e:
+except Exception as e:
     print('An exception as ocurred: ' + str(e))

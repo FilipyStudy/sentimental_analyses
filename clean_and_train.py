1fb1cf641891d80e1ce15837fc413f598ef980f2
FilipyStudy <fiilipydasilvafurtado@gmail.com>
2025-04-19 16:00:11 -0300

    Commit for backup

--- clean_and_train.py
+++ clean_and_train.py
@@ -1,28 +1,32 @@
+#Import the necessary libs
 import pandas as pd
 import matplotlib as plt
-import sklearn
 from sklearn.linear_model import LogisticRegression
 import re
 from pathlib import Path
-import random
 import numpy as np
 import nltk
+ntlk.download('all')
 from nltk.tokenize import word_tokenize
-from nltk.stem import WordNetLemmatizer
+from ntlk.stem import WordNetLemmatizer
 import gc
 import spacy
 import logging
-
 logger = loggin.getLogger(__name__)
+loggin .basicCOnfig(filename = 'cleantrain.log', level=loggin.INFO)
 
+logger.info('Started')
 #Prefer the use of a GPU if it's available
 spacy.prefer_gpu()
 core_lang = spacy.load('en_core_web_lg')
-logger.info("Downloading spacy english core language.")
-#Create a new instance of the WordNetLemmatizer function
+
+#Instanciate the WordNetLemmatizer
 wnl = WordNetLemmatizer()
 
-#Not random stuff
+#Load model
+model = LogisticRegression()
+
+#Patterns for regex.
 DB_PATH = Path('database')
 REGEX_NUMBER = re.compile('[0-3]')
 REGEX_PUNCTUATION = re.compile('[A-Za-z ]')
@@ -40,7 +44,7 @@ def regex_punctuation(x):
     
 #Lemmatize the words
 def lemmatizer(listOfDF):
-    return [wnl.lemmatize(i) for i in listOfDF]
+    return [i for i in listOfDF: wnl.lemmatize(i)]
 
 #Define the lambda function to remove stop words from the text
 remove_stopwords = lambda x: [word for word in x if word not in stopwords]
@@ -48,23 +52,20 @@ remove_stopwords = lambda x: [word for word in x if word not in stopwords]
 #Transform the label into an array of word vector's
 word2vec = lambda x: core_lang(x).vector
 
-#Turn back the list into a String
+#Turn back the list into a string
 turn_into_string = lambda x: " ".join(x)
 
 #Extract, Transform and Load the data. Aka ETL.
 try:
     if DB_PATH.exists():
-        logger.info('Opening train dataset')
         train_file = open(r'database/train_data.txt', 'r')
-        
-        #Open the database
+
         train_dataframe = pd.read_fwf(train_file, sep=',', header=None, encoding='utf-8')
         
         #Close the files and clean the memory to prevent OOM
         train_file.close()
         gc.collect()
 
-    logger.info('Removin NaN values from the train dataset.')
     #Remove NaN column
     train_dataframe = train_dataframe.drop(2, axis=1)
     
@@ -72,41 +73,45 @@ try:
     headers = ['rating', 'review']
     train_dataframe.rename({0: headers[0], 1: headers[1]}, axis=1, inplace=True)
 
+    logger.info('Applying func: regex_number and regex_punctuation')
     #Clean the DataFrame.
-    logger.info("Aplying regex punctuation")
     train_dataframe['rating'] = train_dataframe['rating'].apply(regex_number)
     train_dataframe['review'] = train_dataframe['review'].apply(regex_punctuation)
-    
-    logger.info("Aplying word_tokenize")
 
+    
     #Tokenize the text.
+    logger.info('Applying func: word_tokenize from NLTK')
     train_dataframe['review'] = train_dataframe['review'].apply(word_tokenize)
-     
-    logger.info("Aplying the removal of stop words.")
+    
     #Remove StopWords.
+    logger.info('Applying func: remove_stopwords')
     train_dataframe['review'] = train_dataframe['review'].apply(remove_stopwords)
     
-    logger.info('Aplying lemmatizer')
     #Apply the lemmatization function.
+    logger.info('Applying func: lemmatizer')
     train_dataframe['review'] = train_dataframe['review'].apply(lemmatizer)
-    
-    logger.info('Aplying turn into string function to review column')
+
     #Turn the label back into a String
+    logger.info('Applying func: turn_into_string')
     train_dataframe['review'] = train_dataframe['review'].apply(turn_into_string)
-    
-    logger.info('Aplying word2vec function')
-    #Transform the doc into an array of string vector
+
+    #Transform the doc into an array of string
+    logger.info('Applying func: word2vec')
     train_dataframe['review'] = train_dataframe['review'].apply(word2vec)
-    
-    logger.info('Save the dataset in csv format')
+
     #Save the dataframe as a CSV file for further use.
     train_dataframe.to_csv('train_dataframe.csv', index=False)
     
-    logger.info('opening the train.csv dataset')   
-    if DB_PATH.exitsts():
-        train_file = open(r'train_dataframe.csv', 'r')
-        train_dataframe = pd.read_csv(train_file)
+    #Opening the database for training purpose and saving it
+    logger.info('Opening the database for train purpose')
+    if DB_PATH.exists():
+        train_file = open(r'database/train_data.csv', 'r')
+        train_dataframe = pd.read_csv()
         train_file.close()
 
+    #Fit the data in the model
+    logger.info('Fitting the model')
+    model.fit(train_dataframe [0:], train_dataframe[1:])
+    logger.info('Finished')
 except Exception as e:
-    print('An exception as ocurred: ' + str(e))
+    print(f'An exception as ocurred: {str(e)}')

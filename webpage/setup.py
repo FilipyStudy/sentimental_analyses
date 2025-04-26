f9f2df4a17a09a7f85b30b130a37a22d7bc6a7c4
FilipyStudy <fiilipydasilvafurtado@gmail.com>
2025-04-25 23:07:09 -0300

    modified dockerfile, modified flask files, modified webpage files

--- webpage/setup.py
+++ webpage/setup.py
@@ -1,9 +1,15 @@
 from flask import Flask
 
+
 app = Flask(__name__)
 
 @app.route("/")
+def index():
+    with open('index.html') as f:
+        return f.read()
 
-def hello_word():
-    f = open('index.html', 'r')
-    return f
+@app.route("/style.css")
+def style():
+    with open('style.css') as f:
+        return f.read()
+

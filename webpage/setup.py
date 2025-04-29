b69dbf7fd543869e978debb5eaf6f4a8935e7dae
FilipyStudy <fiilipydasilvafurtado@gmail.com>
2025-04-28 22:12:06 -0300

    added the script.js file to the setup.py

--- webpage/setup.py
+++ webpage/setup.py
@@ -5,11 +5,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    with open('index.html') as f:
+    with open('index.html', 'r') as f:
         return f.read()
+
 
 @app.route("/style.css")
 def style():
-    with open('style.css') as f:
+    with open('style.css', 'r') as f:
+        return f.read()
+
+
+@app.route("/script.js")
+def script():
+    with open('script.js', 'r') as f:
         return f.read()
 
